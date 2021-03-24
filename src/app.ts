5e79c9e9a2f90ab8255487292450073e04bdda00
Antônio Ricart <ricarteusuo11@gmail.com>
2021-03-24 00:57:34 -0300

    env: adiciona estrutura basica do node

---
+++ src/app.ts
@@ -0,0 +1,48 @@
+import express, { Express, json, Request, Response } from 'express';
+import Cors from 'cors';
+import Routes from './routes';
+
+interface AppConfig {
+    PORT?: number
+}
+
+class App {
+
+    public main: Express;
+
+    constructor({ PORT }: AppConfig){
+        this.main = express();
+        this.config();
+        this.connectDatabase();
+        this.routes();
+        this.listen(PORT ? PORT : 3333);
+    }
+
+    private listen(PORT: number): void {
+        this.main.listen(PORT, () => {
+            console.log(`Server is open in port ${PORT}`);
+        });
+    }
+
+    private connectDatabase(): void{
+        // Logic to connect to database
+    }
+
+    private routes(): void{
+        this.main.get('/api/v1', (req: Request, res: Response) => {
+            return res.status(200).json({
+                name: 'pdm-api-atividade-02',
+                version: '1.0.0',
+            });
+        });
+        this.main.use('/api/v1', Routes);
+    }
+
+    private config(): void{
+        this.main.use(json());
+        this.main.use(Cors());
+    }
+
+}
+
+export default App;
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("Começou")
+import App from './app';
+import DotEnv from 'dotenv';
+
+DotEnv.config();
+
+const { PORT } = process.env as any;
+
+class Server {
+
+    static start(){
+        new App({ PORT });
+    }
+}
+
+Server.start();
