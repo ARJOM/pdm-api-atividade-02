c42b313f10ff18d17b4a56e37a2b3497cc828b67
Antônio Ricart <ricarteusuo11@gmail.com>
2021-03-24 22:53:22 -0300

    env: adiciona rotas para todas as entidades

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import AnswerRoutes from './answerRoutes';
+import DiscenteRoutes from './discenteRoutes';
+import SubjectRoutes from './subjectRoutes';
+import SurveyRoutes from './surveyRoutes';
+
 
 const routes = Router();
 
-// preencher com rotas adicionadas
+routes.use(AnswerRoutes);
+routes.use(DiscenteRoutes);
+routes.use(SubjectRoutes);
+routes.use(SurveyRoutes);
 
 export default routes;
---
+++ src/routes/subjectRoutes.ts
@@ -0,0 +1,20 @@
+import { Request, Response, Router } from 'express';
+import { Subject } from '../Model/subject';
+import SubjectService from '../Service/subjectService';
+
+const routes = Router();
+
+
+routes.post('/subjects', (req: Request, res: Response) => {
+    // const subjectService = new SubjectService();
+    // const { workload, name } = req.body;
+    // const subject = new Subject(name, workload);
+    // res.json(subjectService.createSubject(subject));
+});
+
+routes.get('/subjects', (req: Request, res: Response) => {
+    // const subjectService = new SubjectService();
+    // res.json(subjectService.readAllSubject())
+})
+
+export default routes;
