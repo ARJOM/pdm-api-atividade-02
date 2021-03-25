f42201554e043f49cef9a76309bc54a1e19ce48a
caio guilherme de lira dos santos <caioguilhermeuna@gmail.com>
2021-03-25 14:08:57 -0300

    Criando route de survey

--- src/routes/surveyRoutes.ts
+++ src/routes/surveyRoutes.ts
@@ -1,5 +1,14 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import SurveyService from '../Service/surveyService';
+import { Survey } from '../Model/survey';
 
 const routes = Router();
+
+routes.post('/surveys', (req: Request, res: Response) => {
+    const surveyService = new SurveyService();
+    const { title, description} = req.body;
+    const survey = new Survey( title, description);
+    res.json(surveyService.createSurvey(survey));
+});
 
 export default routes;
